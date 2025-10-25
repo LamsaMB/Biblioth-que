3d1eaaa70084b4361020f8cccf5b14a7301702f6
a944187 <a944187@DESKTOP-64LAU1N>
2025-10-25 17:42:57 +0000

    creation du modele auteur et de sa configuration.

---
+++ bibli.DataModel/DTO/Post/CreerAuteurDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bibli.DataModel.DTO.Post
+{
+    internal class CreerAuteurDto
+    {
+    }
+}
